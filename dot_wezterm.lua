026fe1ee4cf16ac108d2dbc827451d0c9f89649e
Evelyn King <evelyn.cameron.king@gmail.com>
2024-09-18 09:54:32 -0400

    Add wezterm configuration file

---
+++ dot_wezterm.lua
@@ -0,0 +1,11 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Actual config
+config.color_scheme = 'Nord'
+config.font = wezterm.font("CaskaydiaCove Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font_size = 13.0
+-- return the configuration to wezterm
+return config
