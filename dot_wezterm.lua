e1becf2b298bf7fbb29441841d7febd93392cbd0
Evelyn King <evelyn.cameron.king@gmail.com>
2024-09-18 09:56:36 -0400

    Messed up nord name

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Actual config
-config.color_scheme = 'Nord'
+config.color_scheme = 'Nord (Gogh)'
 config.font = wezterm.font("CaskaydiaCove Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
 config.font_size = 13.0
 -- return the configuration to wezterm
