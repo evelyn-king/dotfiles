4c4b7c162b72747084c4956e1e8e3a819fa232b4
Evelyn King <evelyn@evieking.com>
2025-01-17 19:21:22 -0800

    Dramatic simplification

--- dot_config/nvim/lua/evelynking/lazy.lua
+++ dot_config/nvim/lua/evelynking/lazy.lua
@@ -13,33 +13,5 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { "shaunsingh/nord.nvim", priority=1000 },
-  {
-    "nvim-telescope/telescope.nvim", tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  "theprimeagen/harpoon",
-  "mbbill/undotree",
-  "tpope/vim-fugitive",
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      {"neovim/nvim-lspconfig"},  -- Required
-      {                           -- Optional
-        "williamboman/mason.nvim",      
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      {"williamboman/mason-lspconfig.nvim"},
-
-      -- Autocompletion
-      {"hrsh7th/nvim-cmp"},       -- Required
-      {"hrsh7th/cmp-nvim-lsp"},   -- Required
-      {"L3MON4D3/LuaSnip"},       -- Required
-    }
-  }
 })
 
