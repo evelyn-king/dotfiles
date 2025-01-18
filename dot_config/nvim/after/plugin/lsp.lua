fd8b7323887ad6b3d21153a25583867f34fa8def
Evelyn King <evelyn@evieking.com>
2025-01-17 19:12:12 -0800

    Trying out the conda lsp

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,25 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+
+local function get_python_path()
+  local conda_env = os.getenv("CONDA_DEFAULT_ENV")
+  if conda_env then
+    return MAMBA_ROOT_PREFIX .. "/envs/" .. conda_env .. "/bin/python"
+  else
+    return "python"
+  end
+end
+
+mason_lsp = require("mason-lspconfig")
+mason_lsp.setup_handlers {
+  function(server_name)
+    if server_name == "pyright" then
+      require("lspconfig").pyright.setup {
+        cmd = { get_python_path(), "-m", "pyright-langserver", "--stdio" },
+      }
+    end
+  end,
+}
+mason_lsp.setup()
 
 local lsp = require("lsp-zero").preset()
 
