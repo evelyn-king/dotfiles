0f23ab08adfc5be797c40c7e3621df206d6205b7
Evelyn King <evelyn@evieking.com>
2025-04-08 14:11:33 -0400

    Revamp of nvim config

--- dot_config/nvim/after/plugin/colors.lua
+++
@@ -1,8 +0,0 @@
-vim.o.background = "dark" -- or "light" for light mode
-function ColorTheScreen(color)
-    color = color or "catppuccin-mocha"
-    vim.cmd.colorscheme(color)
-end
-
-ColorTheScreen()
-
--- dot_config/nvim/lua/evelynking/remap.lua
+++ dot_config/nvim/lua/evelynking/remap.lua
@@ -17,3 +17,12 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Clear highlights on search when pressing <ESC> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", "<cmd>echo \"Use h to move!!\"<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo \"Use l to move!!\"<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo \"Use k to move!!\"<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo \"Use j to move!!\"<CR>")
+
