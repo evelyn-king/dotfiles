d399275bd753493c50ba2febb1ff72219a82554c
Evelyn King <153469488+evelyn-king@users.noreply.github.com>
2025-10-31 17:14:18 -0500

    Apparently I updated my fish config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -5,7 +5,7 @@ if status is-interactive
 end
 
 # >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
+# !! Contents within this block are managed by 'micromamba shell init' !!
 set -gx MAMBA_EXE "/Users/evelynking/.local/bin/micromamba"
 set -gx MAMBA_ROOT_PREFIX "/Users/evelynking/.local/opt/micromamba"
 $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
