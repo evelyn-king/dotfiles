37f00f2450659ec455d7c9d1a91f5a9de076f421
Evelyn King <evelyn@evieking.com>
2026-04-04 09:54:47 -0700

    Handle brew issues

--- dot_config/shell/20_profile_env.sh
+++ dot_config/shell/20_profile_env.sh
@@ -45,6 +45,16 @@ case "${SHELL_OS}:${SHELL_HOSTNAME}" in
     ;;
 esac
 
+if [ "${SHELL_OS}" = "darwin" ]; then
+  if command -v brew >/dev/null 2>&1; then
+    eval "$(brew shellenv)"
+  elif [ -x /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [ -x /usr/local/bin/brew ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
+fi
+
 shell_path_append "${HOME}/.local/bin"
 export PATH
 
